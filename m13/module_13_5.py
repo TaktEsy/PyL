6520d0b65cf959279d64faf5f2eae91d3959d689
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-12-23 22:49:42 +0300

    Row-buttons added

---
+++ m13/module_13_5.py
@@ -0,0 +1,63 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+
+api = ""
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+kb = ReplyKeyboardMarkup()
+math_but = KeyboardButton(text = "Math")
+info_but = KeyboardButton(text = "Info")
+kb.row(math_but,info_but)
+
+
+@dp.message_handler(commands=['start'])
+async def start(mess):
+    await mess.answer('Hi!', reply_markup = kb)
+
+"""
+    1) Это надо наследовать с предыдущего модуля!!!!
+    2) При импорте предыдущего модуля ругается на api ключ (отсутствие)
+"""
+@dp.message_handler(text = "Math")
+async def set_age(message):
+    await message.answer("How much years is you?")
+    await UserState.age.set()
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer("How is growth?")
+    await state.update_data(growth=message.text)
+    await UserState.growth.set()
+
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=message.text)
+    await message.answer("How is weight?")
+    await state.update_data(growth=message.text)
+    await UserState.weight.set()
+
+@dp.message_handler(state = UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight = message.text)
+    data = await state.get_data()
+    m = 10*int(data['weight']) + 6.25*int(data['growth']) - 5*int(data['age']) + 5
+    w = 10*int(data['weight']) + 6.25*int(data['growth']) - 5*int(data['age']) - 161
+
+    await message.answer(f"Норма каллорий (жен): {w}")
+    await message.answer(f"Норма каллорий (муж): {m}")
+    await state.finish()
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
