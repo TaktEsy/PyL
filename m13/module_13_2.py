1aff88ceba99954a8185d17b41eeda9645d8e132
TaktEsy <43720303+TaktEsy@users.noreply.github.com>
2024-12-23 11:26:03 +0300

    Commit

--- m13/module_13_2.py
+++ m13/module_13_2.py
@@ -6,5 +6,19 @@ api = "7442341938:AAH6hyL738xXzGlsNqrO0UFM3z6n8GUBUdc"
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
+
+
+@dp.message_handler(text=["Ur", "ff", 'DD'])
+async def ur_mess(message):
+    print(f'Вы написали ключевое слово {message.text}!')
+
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+    print("Start msg!")
+
+@dp.message_handler()
+async def all_message(message):
+    print("We get some message!")
+
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
